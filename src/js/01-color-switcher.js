fd2ae68bbf96e04f59b93a54f263e36a3bb602f6
Illya-Bilyk <bilikillya.i@gmail.com>
2022-08-21 14:10:40 +0300

    26

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+let colorId = null;
+let bgc = null;
+let isChanging = false;
+
+refs.startBtn.addEventListener('click', onStartBtn);
+refs.stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+  if (isChanging) {
+    return;
+  }
+  colorId = setInterval(() => {
+    bgc = getRandomHexColor();
+    refs.body.style.backgroundColor = bgc;
+    isChanging = true;
+  }, 1000);
+}
+function onStopBtn() {
+  clearInterval(colorId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
